ba857bad5ab61d22e16f8ef7e5c6508134ead2e8
FoveIT <tranvantu7597@gmail.com>
2022-04-01 20:49:03 +0700

    sign up without api

--- src/App.js
+++ src/App.js
@@ -4,31 +4,34 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import RequireAuth from "./components/RequireAuth";
 
-import Home from "./pages/Home";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import SignIn from "./pages/SignInPage";
 
 function App() {
   const theme = createTheme({
     palette: {
-      primary: {
-        light: "#4fb3bf",
-        main: "#00838f",
-        dark: "#005662",
-        contrastText: "#fff",
-      },
-      secondary: {
-        light: "#819ca9",
-        main: "#546e7a",
-        dark: "#29434e",
-        contrastText: "#fff",
-      },
+      // primary: {
+      //   light: "#4fb3bf",
+      //   main: "#00838f",
+      //   dark: "#005662",
+      //   contrastText: "#fff",
+      // },
+      // secondary: {
+      //   light: "#819ca9",
+      //   main: "#546e7a",
+      //   dark: "#29434e",
+      //   contrastText: "#fff",
+      // },
     },
   });
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="sign-in" element={<Home />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="signup" element={<SignUpPage />} />
+          <Route path="signin" element={<SignIn />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
